e98d2bda175f4c414f7660fc71e3c02a26487d29
Giannis Doukas <giannisdoukas2311@gmail.com>
2020-05-04 21:38:19 +0100

    play with build in dot feature

--- cwlkernel/CoreExecutor.py
+++ cwlkernel/CoreExecutor.py
@@ -11,8 +11,11 @@ from typing import (
     NoReturn)
 from uuid import uuid4, UUID
 
-from cwltool.context import RuntimeContext
+from cwltool import process as cwl_process
+from cwltool.context import RuntimeContext, LoadingContext
+from cwltool.cwlrdf import printdot
 from cwltool.factory import Factory
+from cwltool.load_tool import default_loader
 from ruamel import yaml
 
 from .IOManager import IOFileManager
@@ -43,26 +46,38 @@ class CoreExecutor:
         """
         run_id = uuid4()
 
+        data, executable = self.__init_workflow__()
+        try:
+            result: Dict = executable(**data)
+            from io import StringIO
+
+            return run_id, result, None
+        except Exception as e:
+            traceback.print_exc(file=sys.stderr)
+            return run_id, {}, e
+
+    def __init_workflow__(self):
         runtime_context = RuntimeContext()
         runtime_context.outdir = self.file_manager.ROOT_DIRECTORY
         runtime_context.basedir = self.file_manager.ROOT_DIRECTORY
         runtime_context.default_stdin = subprocess.DEVNULL
         runtime_context.default_stdout = subprocess.DEVNULL
         runtime_context.default_stderr = subprocess.DEVNULL
+        loading_context = LoadingContext()
+        loading_context.loader = default_loader(loading_context.fetcher_constructor)
+        with open(self._workflow_path) as f:
+            cwl_version = yaml.load(f)['cwlVersion']
+        loading_context.loader.ctx = cwl_process.get_schema(cwl_version)[0].ctx
+        # process.get_schema(cwlVersion)[:2][0]
         os.chdir(self.file_manager.ROOT_DIRECTORY)
-        factory = Factory(runtime_context=runtime_context)
+        factory = Factory(runtime_context=runtime_context, loading_context=loading_context)
         executable = factory.make(self._workflow_path)
         data = {}
         for data_file in self._data_paths:
             with open(data_file) as f:
                 new_data = yaml.load(f, Loader=yaml.Loader)
                 data = {**new_data, **data}
-        try:
-            result: Dict = executable(**data)
-            return run_id, result, None
-        except Exception as e:
-            traceback.print_exc(file=sys.stderr)
-            return run_id, {}, e
+        return data, executable
 
     @classmethod
     def validate_input_files(cls, yaml_input: Dict, cwd: Path) -> NoReturn:
